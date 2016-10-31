4e4b761f316ec027466bb9acc58cd4c97fee326b
Raphael Zenhäusern <raphael@zenhausern.net>
2016-10-31 09:34:32 +0100

    Initial commit

---
+++ ProjectFlareon/ProjectFlareon/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using ProjectFlareon.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace ProjectFlareon.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
