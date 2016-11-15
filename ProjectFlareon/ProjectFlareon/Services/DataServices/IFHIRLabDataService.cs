64212c290dd1a99692665531283c348bf705cbcc
Raphael Zenhäusern <raphael@zenhausern.net>
2016-11-15 14:06:51 +0100

    Added filtering options to interface

--- ProjectFlareon/ProjectFlareon/Services/DataServices/IFHIRLabDataService.cs
+++ ProjectFlareon/ProjectFlareon/Services/DataServices/IFHIRLabDataService.cs
@@ -10,8 +10,30 @@ namespace ProjectFlareon.Services.DataServices
 {
     public interface IFHIRLabDataService
     {
+        Task<Bundle> PatientsAsync(string snapshotUrl = null);
+        Task<Patient> PatientByIdAsync(string patientId);
+        Task<Organization> OrganizationByIdAsync(string organizationId);
+        Task<Practitioner> PractitionerByIdAsync(string practitionerId);
         Task<Bundle> DiagnosticReportsForPatientAsync(string patId, SummaryType summary = SummaryType.True);
+        Task<Bundle> DiagnosticReportsFilteredForPatientAsync(string patId, DateTimeOffset? issueDate = null, DateTimeOffset? effectiveDate = null, DateTimeOffset? lastUpdateDate = null, DiagnosticReport.DiagnosticReportStatus? status = null, SummaryType summary = SummaryType.True);
+        Task<Bundle> DiagnosticReportsBySnapshotUriAsync(string snapshotUri);
         Task<DiagnosticReport> DiagnosticReportByIdAsync(string reportId, string versionId = null);
         Task<Bundle> DiagnosticReportHistoryByIdAsync(string reportId, SummaryType summary = SummaryType.True);
+        Task<Bundle> ObservationsForPatientAsync(string patId);
+        Task<Observation> ObservationByIdAsync(string observationId);
+
+        // Observation search parameters
+        //patient
+        //Supported
+        //code
+        //Supported
+        //category
+        //Supported
+        //status
+        //Supported
+        //date
+        //Supported
+        //_lastUpdated
+        //Supported
     }
 }
